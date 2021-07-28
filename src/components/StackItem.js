63262438beb0d2e2584527ab575415ca7ee9a7fa
Efren <efrodriguez@redventures.com>
2021-07-28 07:31:21 -0400

    format

--- src/components/StackItem.js
+++ src/components/StackItem.js
@@ -1,66 +1,66 @@
 import { updateStore } from "../store/updateStore";
 
 const StackItem = (state) => {
+  const handleItemEdit = (event) => {
+    const li = event.target.parentElement;
 
-    const handleItemEdit = (event)  => {
-        const li = event.target.parentElement;
+    const itemIndex = state.items.findIndex((item) => item.id === Number.parseInt(li.id));
+    const updatedItems = Object.assign([...state.items], {
+      [itemIndex]: { ...state.items[itemIndex], editing: true },
+    });
 
-        const itemIndex = state.items.findIndex((item) => item.id === Number.parseInt(li.id));
-        const updatedItems = Object.assign([...state.items], {
-           [itemIndex]: { ...state.items[itemIndex], editing: true },
-        });
-    
-        updateStore(state, { items: updatedItems });
-    }
+    updateStore(state, { items: updatedItems });
+  };
 
-    const handleItemSave = (event)  => {
-        const li = event.target.parentElement;
+  const handleItemSave = (event) => {
+    const li = event.target.parentElement;
 
-        const newText = li.querySelector("input").value;
-    
-        const itemIndex = state.items.findIndex((item) => item.id === Number.parseInt(li.id));
-        const updatedItems = Object.assign([...state.items], {
-          [itemIndex]: { ...state.items[itemIndex], editing: false, text: newText },
-        });
-    
-        updateStore(state, { items: updatedItems });
-    }
+    const newText = li.querySelector("input").value;
 
-    const addEventListeners = () => {
-        const addItemEditEventListener = () =>
-            Array.from(document.querySelectorAll("div#stack #itemEdit")).map((el) =>
-                el.addEventListener("click", handleItemEdit)
-            );
+    const itemIndex = state.items.findIndex((item) => item.id === Number.parseInt(li.id));
+    const updatedItems = Object.assign([...state.items], {
+      [itemIndex]: { ...state.items[itemIndex], editing: false, text: newText },
+    });
 
-        const addItemSaveEventListener = () =>
-            Array.from(document.querySelectorAll("div#stack #itemSave")).map((el) =>
-                el.addEventListener("click", handleItemSave)
-            );
+    updateStore(state, { items: updatedItems });
+  };
 
-        return { addItemEditEventListener, addItemSaveEventListener };
-    };
+  const addEventListeners = () => {
+    const addItemEditEventListener = () =>
+      Array.from(document.querySelectorAll("div#stack #itemEdit")).map((el) =>
+        el.addEventListener("click", handleItemEdit)
+      );
 
-    const render = () => {
-        return `
+    const addItemSaveEventListener = () =>
+      Array.from(document.querySelectorAll("div#stack #itemSave")).map((el) =>
+        el.addEventListener("click", handleItemSave)
+      );
+
+    return { addItemEditEventListener, addItemSaveEventListener };
+  };
+
+  const render = () => {
+    return `
         <li id="${state.id}" class="item">
-            ${state.editing === false ?
-                `
+            ${
+              state.editing === false
+                ? `
                     <div>${state.text}</div>
                     <button id="itemEdit">Edit</button>
-                ` : 
                 `
+                : `
                     <input type="text" id="formEdit" value="${state.text}">
                     <button id="itemSave">Save</button>
                 `
             }
         </li>
-        `
-    }
+        `;
+  };
 
-    return {
-        render,
-        addEventListeners
-    }
-}
+  return {
+    render,
+    addEventListeners,
+  };
+};
 
-export { StackItem }
+export { StackItem };
--- src/components/stack.js
+++ src/components/stack.js
@@ -2,52 +2,51 @@ import { updateStore } from "../store/updateStore";
 import { StackItem } from "./StackItem";
 
 const Stack = (state) => {
-    
-    const handleItemAdd = ()  => {
-        const newItem = {
-            id: state.items.length + 1,
-            text: `Item ${state.items.length + 1}`,
-            editing: false
-        }
-
-        updateStore(state, { items: [...state.items, newItem] });
-    }
-
-    const handleItemRemove = ()  => {
-
-        updateStore(state, { items: state.items.filter(i => i.id !== state.items.length) });
-    }
-
-    const addEventListeners = () => {
-        const addItemAddEventListener = () =>
-          document.querySelector("div#stack #itemAdd").addEventListener("click", handleItemAdd);
-
-        const addItemRemoveEventListener = () =>
-          document.querySelector("div#stack #itemRemove").addEventListener("click", handleItemRemove);
-    
-        return { addItemAddEventListener, addItemRemoveEventListener };
+  const handleItemAdd = () => {
+    const newItem = {
+      id: state.items.length + 1,
+      text: `Item ${state.items.length + 1}`,
+      editing: false,
     };
 
-    const render = () => {
-        return `
+    updateStore(state, { items: [...state.items, newItem] });
+  };
+
+  const handleItemRemove = () => {
+    updateStore(state, { items: state.items.filter((i) => i.id !== state.items.length) });
+  };
+
+  const addEventListeners = () => {
+    const addItemAddEventListener = () =>
+      document.querySelector("div#stack #itemAdd").addEventListener("click", handleItemAdd);
+
+    const addItemRemoveEventListener = () =>
+      document.querySelector("div#stack #itemRemove").addEventListener("click", handleItemRemove);
+
+    return { addItemAddEventListener, addItemRemoveEventListener };
+  };
+
+  const render = () => {
+    return `
             <div id="stack">
                 <ul>
-                    ${state.items.map((item) => StackItem(item).render())
-                        .toString()
-                        .replaceAll(",", "")}
+                    ${state.items
+                      .map((item) => StackItem(item).render())
+                      .toString()
+                      .replaceAll(",", "")}
                 </ul>
                 <div id="controls">
                     <button id="itemAdd">Add Item</button>
                     <button id="itemRemove">Remove Item</button>
                 </div>
             </div>
-        `
-    }
+        `;
+  };
 
-    return {
-        render,
-        addEventListeners
-    }
-}
+  return {
+    render,
+    addEventListeners,
+  };
+};
 
-export { Stack }
+export { Stack };
